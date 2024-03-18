515c0702c64e62912663dd5a8661883b3961deb4
svarsha4 <67208746+svarsha4@users.noreply.github.com>
2024-03-18 12:02:37 -0500

    got navigation bar to work

--- website.py
+++ website.py
@@ -2,15 +2,16 @@
 #Sources: 
 #ChatGPT
 #https://youtu.be/5L6h_MrNvsk?si=4uDbgWpGHCEpTEEG
-
-#Issue: The base.css file is not being applied to the base.html file.
+#https://youtu.be/y62Dvo2Ml7o?si=_T4yD_ihGXFhokYS
+#https://getbootstrap.com/docs/5.3/getting-started/introduction/
+#https://www.geeksforgeeks.org/navigation-bars-in-flask/
 
 from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 
 @app.route('/obstacles')
 def obstacles():
@@ -34,7 +35,7 @@ def solutions():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
 
 #What To Do:
 #cd poison-web
