3604b099539e85b20154885c8f2dbd6c77441089
svarsha4 <67208746+svarsha4@users.noreply.github.com>
2024-04-21 15:05:59 -0500

    documented code

--- static/base.js
+++ static/base.js
@@ -1,6 +1,11 @@
+//The below 2 lines of code locate all the tabs in the navigation bar by the data-tab-target tag in HTML.
+//For example, consider the following line of HTML code: <div id="home" class="section1 active" data-tab-content>.
+//Notice, the Home tab has a tag with it called data-tab-content.
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
+
+//The below function makes a given tab active when it is clicked on.
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         console.log(tab)
@@ -17,6 +22,11 @@ tabs.forEach(tab => {
 })
 
 
+//The below function first gets the tag from a given link that gets clicked on.
+//For instance, consdier the following line of code in HTML: <a data-tab-target="#weaknesses" class = "link">Weaknesses</a>.
+//In this case, the tag a[data-tab-target] corresponds to the Weaknesses tab.
+//Once the link gets clicked on, the corresponding tab gets located by its tag (i.e. [data-tab-target="tabName"])
+//and gets set to active.
 document.addEventListener('click', function(event) {
     const target = event.target;
     if (target.matches('a[data-tab-target]')) {
--- website.py
+++ website.py
@@ -9,16 +9,18 @@
 from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
+#This function references the the HTML code in base.html.
 @app.route('/')
 def home():
     return render_template('base.html')
 
 
+#Ensures that the web application is able to run
 if __name__ == "__main__":
     app.run(debug=True) 
 
 
-#What To Do:
+#How to push and pull changes to Git:
 #cd poison-web
 #git add .
 #git commit -m ""
