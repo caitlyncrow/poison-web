0928afbd892f95927ad5d60427a5a239fb7c4cb1
svarsha4 <67208746+svarsha4@users.noreply.github.com>
2024-04-21 13:45:23 -0500

    Fixed remaining website issues

--- static/base.js
+++ static/base.js
@@ -1,7 +1,9 @@
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
+
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
+        console.log(tab)
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('active')
@@ -13,3 +15,21 @@ tabs.forEach(tab => {
         target.classList.add('active')
     })
 })
+
+
+document.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target.matches('a[data-tab-target]')) {
+        event.preventDefault();
+
+        const tabTarget = target.getAttribute('data-tab-target');
+
+        const tab = document.querySelector(`[data-tab-target="${tabTarget}"]`);
+        if (tab) {
+            tab.click();
+        }
+    }
+});
+
+
+
--- website.py
+++ website.py
@@ -13,28 +13,6 @@ app = Flask(__name__)
 def home():
     return render_template('base.html')
 
-'''
-@app.route('/obstacles')
-def obstacles():
-    return render_template('obstacles.html')
-
-@app.route('/identification')
-def identification():
-    return render_template('identification.html')
-
-@app.route('/types')
-def types():
-    return render_template('types.html')
-
-@app.route('/weaknesses')
-def weaknesses():
-    return render_template('weaknesses.html')
-
-@app.route('/solutions')
-def solutions():
-    return render_template('solutions.html')
-'''
-
 
 if __name__ == "__main__":
     app.run(debug=True) 
